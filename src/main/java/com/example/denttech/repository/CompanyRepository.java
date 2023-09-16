a677a133b9409cd83128f44e49a9bc89ac536092
AbdulrahmanShater <106619258+AbdulrahmanShater@users.noreply.github.com>
2023-09-16 19:19:57 +0400

    add company repository

---
+++ src/main/java/com/example/denttech/repository/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.example.denttech.repository;
+
+import com.example.denttech.model.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+
+
+
+}
