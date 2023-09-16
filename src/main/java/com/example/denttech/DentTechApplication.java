afbaa8c49a97ef0d99b3f45f5a8843b794609380
AbdulrahmanShater <106619258+AbdulrahmanShater@users.noreply.github.com>
2023-09-16 12:40:45 +0400

    initial commit

---
+++ src/main/java/com/example/denttech/DentTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.denttech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentTechApplication.class, args);
+    }
+
+}
